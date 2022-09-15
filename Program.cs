2a1166e2b6751716b2d8f940e8e3a66fa2065773
Natali <salamatina_nata@mail.ru>
2022-09-16 02:33:13 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿int[] array = new int [8];
+
+    void FillArray(int[] collection)
+    {
+        int length = collection.Length;
+        int index = 0;
+        while (index < length)
+        {
+            collection[index] = new Random().Next(1,10);
+            index++;
+        }
+    }
+
+
+void PrintArray (int[] collection)
+{
+    int count = collection.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write($"{collection[position]}\t");
+        position++;
+    }
+}
+
+FillArray(array);
+PrintArray(array);

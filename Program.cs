431233373476735d9c3ce0dbdf0dfe9527fef720
Natali <salamatina_nata@mail.ru>
2022-09-16 02:30:38 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumDigit(int num)
+{
+    int count = 0;
+    int result = 0;
+    int temp = 0;
+    while (num > 0)
+    {
+        temp = num % 10;
+        num = (num - temp) / 10;
+        result = result + temp;
+        count++;
+    }
+    return result;
+}
+Console.WriteLine(SumDigit(number));
